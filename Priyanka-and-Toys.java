b02cc32a80d16794c318f47d0a18ac4cf9f6bfcd
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-03-06 03:28:25 +0900

    Update Priyanka-and-Toys.java

--- Priyanka-and-Toys.java
+++ Priyanka-and-Toys.java
@@ -1,1 +1,37 @@
-Priyanka-and-Toys
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'toys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY w as parameter.
+     */
+
+    public static int toys(List<Integer> w) {
+        Collections.sort(w);
+        
+        int count = 0;
+        int limit = Integer.MIN_VALUE;
+        
+        for (int weight : w) {
+            if (weight > limit) {
+                count++;
+                limit = weight + 4;
+            }
+        }
+        
+        return count;
+    }
+}
